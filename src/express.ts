64bc51c9d4eb51883600a608962b2361b12b8473
Aleksi Vuorio <aleksi.vuorio@yahoo.com>
2020-12-07 03:48:13 +0200

    Add helmet

--- src/express.ts
+++ src/express.ts
@@ -2,14 +2,20 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
-// import helmet from "helmet";
+import helmet from "helmet";
 
 import { Config } from "~/config";
+
+const helmetDev = {
+  contentSecurityPolicy: false as const,
+};
 
 export const createExpress = (opts: { config: Config }) => {
   const app = express();
 
-  // app.use(helmet({ referrerPolicy: { policy: "no-referrer" } }));
+  // https://github.com/helmetjs/helmet
+  app.use(helmet(opts.config.isProduction ? undefined : helmetDev));
+
   app.use(cors({ origin: opts.config.apiCorsEndpoint, credentials: true }));
   app.use(cookieParser());
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +23,7 @@ export const createExpress = (opts: { config: Config }) => {
   return app;
 };
 /*
+
 import {
   HealthChecker,
   LivenessEndpoint,
@@ -24,11 +31,9 @@ import {
   HealthEndpoint,
 } from "@cloudnative/health-connect";
 
-const healthcheck = new HealthChecker();
-  const app = express();
-
+  const healthcheck = new HealthChecker();
   app.use("/live", LivenessEndpoint(healthcheck));
   app.use("/ready", ReadinessEndpoint(healthcheck));
   app.use("/health", HealthEndpoint(healthcheck));
 
-  */
+*/
