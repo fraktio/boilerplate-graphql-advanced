c3b95619931a6e0e164cb66b8f1817a3c315649e
Henri Vesala <henri.vesala@gmail.com>
2021-09-14 02:25:14 +0300

    fixed tests

--- app/graphql/__tests__/apolloServer.test.ts
+++ app/graphql/__tests__/apolloServer.test.ts
@@ -1,11 +1,15 @@
+import { createServer } from "http";
+
 import { createApolloServer } from "~/graphql/apolloServer";
 import { createTestConfig } from "~/tests/testConfig";
 
 const config = createTestConfig();
 
+const httpServer = createServer();
+
 describe("Graphql / endpoints", () => {
   it("logout", async () => {
-    const server = createApolloServer({ config });
+    const server = createApolloServer({ config, httpServer });
 
     expect(server.constructor.name).toBe("ApolloServer");
   });
